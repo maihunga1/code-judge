fa99a9a249b24c4de7b5944015bcbf891f53a64a
Khoi Tuan <khoi@desygner.com>
2024-10-24 22:28:01 +1000

    lambda done

--- code-judge-backend/lambda-userService/index.js
+++ code-judge-backend/lambda-userService/index.js
@@ -1,11 +1,12 @@
 const mysql = require("mysql2/promise");
 const dotenv = require("dotenv");
 
-// can't use ssm due to security groups config, can't be bothered to fix
 dotenv.config();
 
 exports.handler = async (event) => {
   try {
+    const userID = event.requestContext.authorizer.jwt.claims.sub;
+
     const connection = await mysql.createConnection({
       host: process.env.HOST,
       user: process.env.USER,
@@ -14,12 +15,27 @@ exports.handler = async (event) => {
       port: process.env.PORT,
     });
 
-    await connection.ping();
+    const [rows] = await connection.execute(
+      "SELECT * FROM users WHERE userID = ? LIMIT 1",
+      [userID]
+    );
+
     await connection.end();
 
+    if (rows.length === 0) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify("User not found"),
+      };
+    }
+
+    const data = rows[0];
+
     return {
-      statusCode: 200,
-      body: JSON.stringify("Connection to RDS was successful!"),
+      userID: data.userID,
+      email: data.email,
+      username: data.username,
+      created: data.created,
     };
   } catch (error) {
     console.error("Error connecting to the RDS:", error);
